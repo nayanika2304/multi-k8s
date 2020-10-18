695e6e532cf55bbe46147d8270c5b8ed97b063a5
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-10-18 14:18:09 -0400

    fixes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-docker build -t nayanika2304/multi-client:latest -t nayanika2304/multi-client:$SHA -f ./client/Dockerfile.dev ./client
-docker build -t nayanika2304/multi-server:latest -t nayanika2304/multi-server:$SHA -f ./server/Dockerfile.dev ./server
-docker build -t nayanika2304/multi-worker:latest -t nayanika2304/multi-worker:$SHA -f ./worker/Dockerfile.dev ./worker
+
+docker build -t nayanika2304/multi-client:latest -t nayanika2304/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t nayanika2304/multi-server:latest -t nayanika2304/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t nayanika2304/multi-worker:latest -t nayanika2304/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push nayanika2304/multi-client:latest
 docker push nayanika2304/multi-server:latest
@@ -10,7 +11,7 @@ docker push nayanika2304/multi-client:$SHA
 docker push nayanika2304/multi-server:$SHA
 docker push nayanika2304/multi-worker:$SHA
 
-kubectll apply -f k8s
+kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=nayanika2304/multi-server:$SHA
 kubectl set image deployments/client-deployment client=nayanika2304/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=nayanika2304/multi-worker:$SHA
